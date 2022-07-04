4d96edb623a159d38744e5f8bdb89589dc2e183c
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-04 18:10:16 -0300

    Initial commit

---
+++ src/main/java/com/alice/renna/RennaApplication.java
@@ -0,0 +1,13 @@
+package com.alice.renna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RennaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RennaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alice/renna/RennaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.renna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RennaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
