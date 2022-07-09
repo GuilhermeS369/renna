46d9b0c572784e7c93a2e85af3a3c6e3499ced3c
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-08 23:39:18 -0300

    Obtendo um usuário por Id
    Já incluso tratamento de exceção

--- src/main/java/com/alice/renna/resource/UserResource.java
+++ src/main/java/com/alice/renna/resource/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,36 @@ public class UserResource {
  		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+	
+		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/alice/renna/services/UserService.java
+++ src/main/java/com/alice/renna/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alice.renna.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alice.renna.domain.User;
 import com.alice.renna.repository.UserRepository;
+import com.alice.renna.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,33 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
