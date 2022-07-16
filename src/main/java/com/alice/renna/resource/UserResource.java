9e82ecf17ac85abf6afed8be744df8ce41807938
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-16 12:07:48 -0300

    Atualização de usuário com PUT

--- src/main/java/com/alice/renna/resource/UserResource.java
+++ src/main/java/com/alice/renna/resource/UserResource.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +53,24 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@DeleteMapping(value ="/{id}")
+	public ResponseEntity<Void> delete (@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
 	
+	@PutMapping(value ="/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		//CONVERSÃO DE DTO PARA ENTIDADE COMUM:
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		
+		//METODO QUE INSERE NO BANCO
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
 
 
--- src/main/java/com/alice/renna/services/UserService.java
+++ src/main/java/com/alice/renna/services/UserService.java
@@ -34,6 +34,23 @@ public class UserService {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
+	public void delete(String id) {
+		repo.deleteById(id);
+		
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+		
+	}
 }
 
 
