951e78756b36499cfe9c0b62c19a8ca88a569945
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-18 21:47:06 -0300

    Consulta simples com query methods

--- src/main/java/com/alice/renna/repository/PostRepository.java
+++ src/main/java/com/alice/renna/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.alice.renna.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,9 @@ import com.alice.renna.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+		
 	
 	
 }
--- src/main/java/com/alice/renna/resource/PostResource.java
+++ src/main/java/com/alice/renna/resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.alice.renna.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alice.renna.domain.Post;
+import com.alice.renna.resource.util.URL;
 import com.alice.renna.services.PostService;
 
 @RestController
@@ -22,6 +26,17 @@ public class PostResource {
 		Post post = service.findById(id);
 	
 		return ResponseEntity.ok().body(post);
+	}
+	
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		
+		text = URL.decodeParam(text);
+					
+		List<Post> list = service.findByTitle(text);
+	
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
