40639fbb9e6ee1a673139f4c435dd4f6d961e0b6
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-18 22:11:15 -0300

    Consulta simples com @Query

--- src/main/java/com/alice/renna/repository/PostRepository.java
+++ src/main/java/com/alice/renna/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.alice.renna.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alice.renna.domain.Post;
@@ -11,7 +12,9 @@ import com.alice.renna.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-		
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle (String text);
 	
 	
 }
