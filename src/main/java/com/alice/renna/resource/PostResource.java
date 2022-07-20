c7aa64d374656c30dff3d7bac7c0a82e8c90258c
Guilherme Santos <guilherme460@yahoo.com.br>
2022-07-19 21:53:51 -0300

    Consulta com vários critérios

--- src/main/java/com/alice/renna/resource/PostResource.java
+++ src/main/java/com/alice/renna/resource/PostResource.java
@@ -1,5 +1,6 @@
 package com.alice.renna.resource;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,20 @@ public class PostResource {
 		text = URL.decodeParam(text);
 					
 		List<Post> list = service.findByTitle(text);
+	
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.fullSearch(text, min, max);
 	
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/alice/renna/resource/util/URL.java
+++ src/main/java/com/alice/renna/resource/util/URL.java
@@ -2,6 +2,10 @@ package com.alice.renna.resource.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	public static String decodeParam(String text) {
@@ -11,5 +15,15 @@ public class URL {
 			return "";
 		}
 		
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
 	}
 }
